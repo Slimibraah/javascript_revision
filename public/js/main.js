4e6c102ecbe7ce1f47f6f81c89db8273a4adc909
Slimibraah <duclos.salim@gmail.com>
2021-02-02 11:08:17 +0100

    Java Revision Exo3

---
+++ public/js/main.js
@@ -0,0 +1,94 @@
+// Exo1
+// 01
+//let prenom = prompt("Quelle est ton prénom?")
+//alert(prenom.charAt(0))
+// 02
+//alert(prenom.substring(1))
+// 03
+// let prenom = prompt("Quelle est ton prenom?")
+
+// if (prenom.length >= 5) {
+//     alert(prenom.charAt(4))
+// }
+// else {
+//     alert("ton prenom est pas assez long")
+// }
+// let nom = prompt("Nom ?")
+// if (nom.length >= 5) {
+//     alert("Le 5éme lettre " + nom.charAt(4))
+    
+// }else {
+//     alert(nom + "Le dernier caractere de votre prenom" + nom.substring( 1) + ",votre prenom est trop court , il manque" + nom.length + "Caractere")
+// }
+//
+// let nom = prompt("Ton nom ?")
+// alert(nom.toLowerCase)
+// alert(nom.toUpperCase)
+// alert(nom.charAt(0).toUpperCase()+nom.substr(1,nom.length-1).toLowerCase())
+
+// Exo2
+
+
+// let tableau = [
+//     "Salim",
+//     "Abdel",
+//     "Amine",
+//     "Mufasa",
+//     "Gauthier"
+// ]
+
+
+//console.log(tableau[2])
+//console.log(tableau[0],tableau[table.lenght-1])
+//console.log(tableau[2].substring(0, 4).toUpperCase()+tableau[2].substring(4))
+//console.log(tableau[4].slice(1));
+//console.log(tableau[0].charAt(0).toLowerCase()+tableau[0].charAt(1).toUpperCase());
+//console.log(tableau[0]+ " " + tableau[2]);
+//console.log(tableau[1].substring(0, 3)+tableau[4].substring(0, 3));
+//console.log(tableau[2].substring(0, 3).toUpperCase()+tableau[4])
+
+// EXO3
+
+//1. Créer un tableau avec 9 prénoms
+let tableau = ["Mohammed", "Yasmina", "Amine", "Salim", "Abdel", "Majda", "Bilal", "Laurie","yahya"]
+//2. Affiche chaque élément dans un console log
+
+/* tableau.forEach(element => {
+    console.log(element)
+}); /
+
+//3. Affiche chaque premiere lettre de chaque élément dans un console log
+
+/ tableau.forEach(element =>  {
+    console.log(element.charAt(0))
+}); /
+
+//4. Affiche chaque seconde lettre de chaque élément dans un console log
+
+/ tableau.forEach(element => {
+    console.log(element.charAt(1));
+}); /
+
+//5. Affiche chaque seconde lettre de chaque élément concaténé a tout le mot lui meme exemple : si le mot et bonjour alors on devra affiché obonjour
+
+/ tableau.forEach(element => {
+    console.log(element.charAt(1)+element);
+}); /
+
+//6. Affiche tous les prénoms dans un console log dont l'indice est pair 
+
+/ tableau.forEach(element => {
+
+    if (tableau.indexOf(element) %2 == 0) {
+        console.log(element);
+    } 
+}); /
+
+// ou
+
+/ tableau.forEach((element,index) => {
+
+    if (index %2 == 0) {
+        console.log(element);
+    } 
+}); */
